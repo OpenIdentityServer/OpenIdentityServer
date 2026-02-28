48f847fe26f62ea4637973113a451ed26827bfcb
2pNza <2pNza@protonmail.com>
2026-02-28 04:13:09 +0200

    add SPDX-License-Identifier to all sh files

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
 git clean -xdf -e samples -e src/OpenIdentityServer/.vs -e .idea
 
 ./clean_cache.sh
--- clean_cache.sh
+++ clean_cache.sh
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
 rm -rf ./nuget
 
 rm -rf ~/.nuget/packages/openidentityserver
